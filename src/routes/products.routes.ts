13813b1cc2d99cb198314f95aa61bd8529a38a4b
joshticks002 <jadeyemo002@gmail.com>
2023-01-09 08:56:48 +0100

    Upload file service

--- src/routes/products.routes.ts
+++ src/routes/products.routes.ts
@@ -1,16 +1,16 @@
-import express, { Request, Response, NextFunction } from "express";
+import express from "express";
 const productRouter = express.Router();
 import * as Product from "../controllers/products.controller";
 import validateProductData from "../middleware/validate-product-data";
 import { validateRequestParameter } from "../middleware/validate-request-data";
+import MoveFile from "../middleware/move-file";
+import multerMemory from "../middleware/upload-file";
 import validateFileUpload from "../middleware/validate-file-upload";
-import storage from "../utils/cloud-storage";
-import multer from "multer";
-const uploads = multer({ storage, limits: { fileSize: 1 * 1024 * 1024 } });
 
 productRouter.post(
   "/products",
-  uploads.single("file"),
+  multerMemory.single("file"),
+  MoveFile.awsMove,
   validateFileUpload,
   validateProductData,
   Product.addProduct
